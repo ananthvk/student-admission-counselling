3820188d892d5a2463e86668210f88663f64b859
ananthvk <123456789987654321sr@gmail.com>
2024-09-19 23:22:21 +0530

    Wrote a few more tests

--- matcher/tests/test_da_algorithm.cpp
+++ matcher/tests/test_da_algorithm.cpp
@@ -81,13 +81,37 @@ TEST(Matching, SingleCourseMultipleStudent)
 
 TEST(Matching, MultipleCourseMultipleStudent)
 {
-    RankList main_rank_list({1});
-    std::vector<Course> courses = {Course(main_rank_list, 1)};
-    std::vector<Student> students = {Student({0}, 0)};
+    std::vector<int> ranks = {3, 5, 4, 1, 2, 8, 10, 7, 9, 6, 11};
+    RankList main_rank_list(ranks);
+    std::vector<Course> courses = {Course(main_rank_list, 2), Course(main_rank_list, 1),
+                                   Course(main_rank_list, 3), Course(main_rank_list, 4)};
+    
+    std::vector<Student> students = {
+        Student({2}, 0),             // 0 3 
+        Student({2, 0}, 1),          // 1 5
+        Student({2, 1, 0}, 2),       // 2 4
+        Student({2, 3, 1}, 3),       // 3 1
+        Student({2, 3, 1}, 4),       // 4 2
+        Student({2, 3, 1}, 5),       // 5 8
+        Student({0, 1, 2}, 6),       // 6 10
+        Student({0, 1, 3}, 7),       // 7 7
+        Student({0, 2, 1, 3}, 8),    // 8 9
+        Student({0, 3, 1, 2}, 9),    // 9 6
+        Student({2, 0, 1, 3}, 10),   // 10 11
+    };
+    
     GaleShapley::perform_allotment(students, courses);
-    EXPECT_EQ(students[0].get_alloted_course_id(), 0);
-    EXPECT_EQ(courses[0].get_last_alloted_rank(), 1);
-    EXPECT_EQ(courses[0].get_last_alloted_student(), 0);
+    EXPECT_EQ(students[0].get_alloted_course_id(), 2);
+    EXPECT_EQ(students[1].get_alloted_course_id(), 0);
+    EXPECT_EQ(students[2].get_alloted_course_id(), 1);
+    EXPECT_EQ(students[3].get_alloted_course_id(), 2);
+    EXPECT_EQ(students[4].get_alloted_course_id(), 2);
+    EXPECT_EQ(students[5].get_alloted_course_id(), 3);
+    EXPECT_EQ(students[6].get_alloted_course_id(), -1);
+    EXPECT_EQ(students[7].get_alloted_course_id(), 3);
+    EXPECT_EQ(students[8].get_alloted_course_id(), 3);
+    EXPECT_EQ(students[9].get_alloted_course_id(), 0);
+    EXPECT_EQ(students[10].get_alloted_course_id(), 3);
 
 }
 
