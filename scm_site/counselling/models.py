fd5c97db4b9a9d9f5c77569fddcc3f953b202d19
ananthvk <123456789987654321sr@gmail.com>
2024-09-22 04:08:01 +0530

    Completed a few models

--- scm_site/counselling/models.py
+++ scm_site/counselling/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.TextField()
+    code = models.TextField()
+
+
+class College(models.Model):
+    name = models.TextField()
+    address = models.TextField()
+    contact_email = models.EmailField()
+    college_type = models.TextField()
+    code = models.TextField()
+    programs = models.ManyToManyField(Course, through="Program")
+
+
+class RankList(models.Model):
+    short_name = models.TextField()
+    name = models.TextField()
+
+
+class Program(models.Model):
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    total_seats = models.PositiveIntegerField()
+    ranklist = models.ForeignKey(RankList, on_delete=models.RESTRICT)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["college", "course"],
+                name="course_cannot_be_repeated_in_college",
+            )
+        ]
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+
+
+class RankListEntry(models.Model):
+    ranklist = models.ForeignKey(RankList, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    rank = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["ranklist", "student"], name="student_unique_in_ranklist"
+            )
+        ]
+
+
+class ChoiceEntry(models.Model):
+    priority = models.IntegerField(default=0)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["student", "program"], name="student_program_unique"
+            )
+        ]
