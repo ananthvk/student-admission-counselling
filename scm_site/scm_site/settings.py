b3f3cb82450bc5cbb4dc82ca6dc8d1216ad9a81d
ananthvk <123456789987654321sr@gmail.com>
2024-09-26 01:58:52 +0530

    Added more styles

--- scm_site/scm_site/settings.py
+++ scm_site/scm_site/settings.py
@@ -142,3 +142,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "counselling:index"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
--- scm_site/scm_site/urls.py
+++ scm_site/scm_site/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('counselling/', include('counselling.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('accounts/', include("django.contrib.auth.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
