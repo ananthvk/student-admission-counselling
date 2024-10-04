995b23ce1213c0ef00a194646a22257735faeb8a
ananthvk <123456789987654321sr@gmail.com>
2024-10-05 02:36:09 +0530

    Added choice saving

--- scm_site/counselling/urls.py
+++ scm_site/counselling/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("college/list/", views.CollegeListView.as_view(), name="college_list"),
     path("course/list/", views.CourseListView.as_view(), name="course_list"),
     path("choiceentry/", views.option_entry_view, name="choice_entry"),
-    path("api/college/<int:college_id>/programs", views.get_programs_offered_by_college, name="get_programs_offered_by_college")
+    path("api/choiceentry", views.option_entry_post, name="choice_entry_post"),
+    path("api/college/<int:college_id>/programs", views.get_programs_offered_by_college, name="get_programs_offered_by_college"),
 ]
--- scm_site/counselling/views.py
+++ scm_site/counselling/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, get_object_or_404
-from .models import College, Course
+from .models import College, Course, Program, Student, ChoiceEntry
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
-
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.db import transaction
+import json
 
 # Create your views here.
 
@@ -31,12 +33,30 @@ class CollegeDetailView(DetailView):
 
 
 @login_required
+@ensure_csrf_cookie
 def option_entry_view(request: HttpRequest):
     # Only return those colleges which offer atleast one program
     queryset = (
         College.objects.filter(programs__isnull=False).distinct().order_by("code")
     )
-    return render(request, "counselling/choice_entry.html", {"colleges": queryset})
+    # Also pass in the options filled by this candidate, if they want to update it
+    student = Student.objects.get(user=request.user)
+    choices = ChoiceEntry.objects.filter(student=student).order_by("priority")
+    return render(request, "counselling/choice_entry.html", {"colleges": queryset, "choices": choices})
+
+
+@login_required
+@require_http_methods(["POST"])
+def option_entry_post(request: HttpRequest):
+    payload = json.loads(request.body)
+    student = Student.objects.get(user=request.user)
+    with transaction.atomic():
+        for priority_number, college_id, course_id in payload:
+            program = Program.objects.get(college_id=college_id, course_id=course_id)
+            choice_entry = ChoiceEntry(student=student, program=program, priority=priority_number)
+            choice_entry.save()
+        
+    return JsonResponse({"status": "ok"})
 
 
 @require_http_methods(["GET"])
@@ -44,5 +64,9 @@ def get_programs_offered_by_college(request: HttpRequest, college_id):
     # program_codes = get_object_or_404(College, pk=college_id).programs.order_by('code').values('code')
     program_codes = get_object_or_404(College, pk=college_id).programs.order_by("code")
     return JsonResponse(
-        {"courses": [{"id": x.id, "code": x.code, "name": x.name} for x in program_codes]}
+        {
+            "courses": [
+                {"id": x.id, "code": x.code, "name": x.name} for x in program_codes
+            ]
+        }
     )
