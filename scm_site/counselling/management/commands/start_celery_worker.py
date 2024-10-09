399f7dc5c7fe4b090ef5ed73383a89d428008a78
ananthvk <123456789987654321sr@gmail.com>
2024-10-09 18:03:27 +0530

    Added celery worker to docker

---
+++ scm_site/counselling/management/commands/start_celery_worker.py
@@ -0,0 +1,19 @@
+import shlex
+import subprocess
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+def restart_celery():
+    cmd = 'pkill -f "celery worker"'
+    subprocess.call(shlex.split(cmd))
+    cmd = 'watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- celery -A scm_site worker --loglevel=INFO'
+    #cmd = 'python -m celery -A scm_site worker -l info'
+    subprocess.call(shlex.split(cmd))
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print('Starting celery worker....')
+        # autoreload.run_with_reloader(restart_celery)
+        restart_celery()
