156027bdc419d71ed8036c894e954cd6adb5df96
ananthvk <123456789987654321sr@gmail.com>
2024-10-13 03:08:02 +0530

    Added unfold for admin ui

--- counselling/admin.py
+++ counselling/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from preferences.admin import PreferencesAdmin
 from .models import Course, College, ChoiceEntry, Program, RankList, RankListEntry, Student, SitePreference
 # Register your models here.
 
-admin.site.register(Course)
-admin.site.register(College)
-admin.site.register(ChoiceEntry)
-admin.site.register(Program)
-admin.site.register(RankList)
-admin.site.register(RankListEntry)
-admin.site.register(Student)
+
+@admin.register(Course)
+class CourseAdmin(ModelAdmin):
+    pass
+
+@admin.register(College)
+class CollegeAdmin(ModelAdmin):
+    pass
+
+@admin.register(ChoiceEntry)
+class ChoiceEntryAdmin(ModelAdmin):
+    pass
+
+@admin.register(Program)
+class ProgramAdmin(ModelAdmin):
+    pass
+
+@admin.register(RankList)
+class RankListAdmin(ModelAdmin):
+    pass
+
+@admin.register(RankListEntry)
+class RankListEntryAdmin(ModelAdmin):
+    pass
+
+@admin.register(Student)
+class StudentAdmin(ModelAdmin):
+    pass
 
 admin.site.register(SitePreference, PreferencesAdmin)
+class PreferencesAdminClass(ModelAdmin):
+    pass
--- scm_site/settings.py
+++ scm_site/settings.py
@@ -54,6 +54,13 @@ LOGGING = {
 
 INSTALLED_APPS = [
     "django_browser_reload",
+    "unfold",  # before django.contrib.admin
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",  # optional, if django-guardian package is used
+    "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
     "counselling.apps.CounsellingConfig",
     "django.contrib.admin",
     "django.contrib.auth",
